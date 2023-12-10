83452d1376558c3b63caa64cdffc96a7c49edade
Roberto <roberto.geiss.nh@icloud.com>
2023-12-10 11:13:23 -0300

    Initial Commit

---
+++ FinancasPessoal/FinancasPessoalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancasPessoalApp.swift
+//  FinancasPessoal
+//
+//  Created by Roberto Edgar Geiss on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FinancasPessoalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
