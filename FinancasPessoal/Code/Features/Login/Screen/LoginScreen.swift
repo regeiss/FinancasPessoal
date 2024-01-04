d5839cd370136dadf00493661c17e17df8ca4611
Roberto <roberto.geiss.nh@icloud.com>
2024-01-03 21:54:54 -0300

    Navegacao inicial e tela de login

---
+++ FinancasPessoal/Code/Features/Login/Screen/LoginScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LoginScreen.swift
+//  FinancasPessoal
+//
+//  Created by Roberto Edgar Geiss on 02/01/24.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginScreen()
+}
---
+++ FinancasPessoal/Code/Features/Settings/Screen/SettingsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsScreen.swift
+//  FinancasPessoal
+//
+//  Created by Roberto Edgar Geiss on 02/01/24.
+//
+
+import SwiftUI
+
+struct SettingsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsScreen()
+}
